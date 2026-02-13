985e205fe50e0369c7a50df07c1898f0f3739cec
sotliqovjavohir4151-droid <sotliqovjavohir4151@gmail.com>
2026-02-13 19:44:45 +0500

    Initialize bot.py with basic bot setup

---
+++ bot.py
@@ -0,0 +1,11 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(token=8564026272:AAGFcUZO7hK8OhX4RIZoy4Ap8be5eYa-6PI)
+dp = Dispatcher()
+Commit changes
