2abd5d7c5955c9819da3c2a2e0df271c63a7de77
sotliqovjavohir4151-droid <sotliqovjavohir4151@gmail.com>
2026-02-13 20:28:30 +0500

    Update bot.py with hardcoded token
    Replaced environment variable token with hardcoded token.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("8564026272:AAGFcUZO7hK8OhX4RIZoy4Ap8be5eYa-6PI")
 
 bot = Bot(token=8564026272:AAGFcUZO7hK8OhX4RIZoy4Ap8be5eYa-6PI)
 dp = Dispatcher()
